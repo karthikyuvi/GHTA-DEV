71fd074a4929c1ee99274fab13beee1327f23855
RTG8055 <vkrahulagarwal@gmail.com>
2021-11-15 00:53:01 -0500

    linked new table to values, added change % and val, added ipov back

--- test.py
+++ test.py
@@ -3,6 +3,17 @@ from requests.auth import HTTPDigestAuth
 import json
 import os
 
+
+js_loop = "for( i=0 ;i< document.getElementsByClassName('{0}').length;i++){{ document.getElementsByClassName('{0}')[i].innerHTML = '{1}'; }}"
+def js_line(value1, value2):
+	try:
+		l = js_loop.format(value1, value2)
+		return l
+	except Exception as e:
+		print(e)
+		print(values[0], " Not updated!!")
+	return " "
+
 api_username = os.environ.get('API_USERNAME')
 api_password = os.environ.get('API_PASSWORD')
 session = requests.Session()
@@ -19,23 +30,43 @@ if r_content['response']['status_description'] != 'Success':
 date_of_info = list(r_content['data'].keys())[0]
 fund_info = r_content['data'][date_of_info][0]
 
-js_loop = "for( i=0 ;i< document.getElementsByClassName('{0}').length;i++){{ document.getElementsByClassName('{0}')[i].innerHTML = '{1}'; }}"
+
+
+
+prev_nav = 0
+with open("assets/old_cache.txt",'r') as f:
+	for line in f:
+		prev_nav = float(line.strip("\n|\r"))
 
 with open("assets/js/etf1_python.js",'w') as f:
-	f.write(js_loop.format('fund_ticker',fund_info['fund_ticker']))
-	f.write(js_loop.format('fund_description',fund_info['fund_description']))
-	f.write(js_loop.format('fund_iopv',fund_info['iopv_symbol']))
-	f.write(js_loop.format('fund_exchg',fund_info['fund_listing_exchange']))
-	f.write(js_loop.format('fund_cusip',fund_info['fund_cusip']))
-	f.write(js_loop.format('fund_inception_date',fund_info['inception_date']))
-	f.write(js_loop.format('fund_net_assets',"${:,.2f}".format(float(fund_info['fund_shares_outstanding'])*float(fund_info['nav']))))
-	f.write(js_loop.format('fund_shares_ots',"{:,}".format(fund_info['fund_shares_outstanding'])))
+	f.write(js_line('fund_ticker',fund_info['fund_ticker']))
+	f.write(js_line('fund_description',fund_info['fund_description']))
+	f.write(js_line('fund_iopv',fund_info['iopv_symbol']))
+	f.write(js_line('fund_exchg',fund_info['fund_listing_exchange']))
+	f.write(js_line('fund_cusip',fund_info['fund_cusip']))
+	f.write(js_line('fund_inception_date',fund_info['inception_date']))
+	f.write(js_line('fund_net_assets',"${:,.2f}".format(float(fund_info['fund_shares_outstanding'])*float(fund_info['nav']))))
+	f.write(js_line('fund_shares_ots',"{:,}".format(fund_info['fund_shares_outstanding'])))
 
-	f.write(js_loop.format('fund_info_date',date_of_info))
-	f.write(js_loop.format('fund_nav',fund_info['nav']))
-	f.write(js_loop.format('fund_close_price',fund_info['market_price']))
-	f.write(js_loop.format('fund_discount_premium',fund_info['discount_premium']))
-	f.write(js_loop.format('fund_thirty_day_median_bid_ask',fund_info['thirty_day_median_bid_ask']))
+	f.write(js_line('fund_info_date',date_of_info))
+	f.write(js_line('fund_nav',fund_info['nav']))
+	f.write(js_line('fund_close_price',fund_info['market_price']))
+	f.write(js_line('fund_discount_premium',fund_info['discount_premium']))
+	f.write(js_line('fund_thirty_day_median_bid_ask',fund_info['thirty_day_median_bid_ask']))
+
+	f.write(js_line('fund_chg_dollar', float(fund_info['nav']) - prev_nav))
+	f.write(js_line('fund_chg_percent', (float(fund_info['nav']) - prev_nav)/prev_nav))
+
+	f.write(js_line('premium_discount_days_q', fund_info['premium_discount_days_q'] ))
+	f.write(js_line('premium_discount_days_y', fund_info['premium_discount_days_y'] ))
+
+	f.write(js_line('current_cal_q_pd_premium', fund_info['current_cal_q_pd_premium'] ))
+	f.write(js_line('current_cal_q_pd_discount', fund_info['current_cal_q_pd_discount'] ))
+	f.write(js_line('current_cal_q_pd_total', fund_info['current_cal_q_pd_total'] ))
+
+	f.write(js_line('previous_cal_y_pd_premium', fund_info['previous_cal_y_pd_premium'] ))
+	f.write(js_line('previous_cal_y_pd_discount', fund_info['previous_cal_y_pd_discount'] ))
+	f.write(js_line('previous_cal_y_pd_total', fund_info['previous_cal_y_pd_total'] ))
 
 	holding_tr = ""
 	for i in range(0,len(fund_info['constituents'])):
@@ -49,3 +80,6 @@ with open("assets/js/etf1_python.js",'w') as f:
 	'''
 	f.write('document.getElementById("fund_holdings_table").innerHTML += "{}";'.format(holding_tr))
 	# print(holding_tr)
+
+with open("assets/old_cache.txt",'w') as f:
+	f.write(str(fund_info['nav']))
